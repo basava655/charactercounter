2b545bd7df3efc4fd8ee000be90c718d2188c215
basava655 <basavau655@gmail.com>
2023-09-13 15:46:29 +0530

    charactercounter

---
+++ charactercounter.js
@@ -0,0 +1,14 @@
+const textareaEl = document.getElementById("textarea");
+const totalcounterEl = document.getElementById("total-counter");
+const remainingcounterEl = document.getElementById("remaining-counter");
+
+textareaEl.addEventListener("keyup", () => {
+    updatecounter();
+});
+
+updatecounter()
+
+function updatecounter(){
+    totalcounterEl.innerText = textareaEl.value.length;
+    remainingcounterEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+}
